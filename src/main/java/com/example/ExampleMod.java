10c38014a47e2d126b93dfa8c2b859dc1e33abeb
Clément Prod'homme <clement.prod-homme@captive.fr>
2024-05-26 20:53:08 +0200

    ♻️ Refactor ExampleMod

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,13 +1,6 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.block.*;
-import net.minecraft.item.*;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
-
-// import com.example.MischievousSkullBlock.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,8 +11,8 @@ public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final String MOD_ID = "examplemod";
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "examplemod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
@@ -30,15 +23,5 @@ public class ExampleMod implements ModInitializer {
 		LOGGER.info("Initalize : " + MOD_ID);
 
 		ModBlocks.registerModBlock();
-
-		// Registry.register(
-		// 	Registries.BLOCK,
-		// 	new Identifier("tutorial", "mischievous_skull_block"), MISCHIEVOUS_SKULL_BLOCK
-		// );
-		// Registry.register(
-		// 	Registries.ITEM,
-		// 	new Identifier("tutorial", "mischievous_skull_block"), 
-		//  new BlockItem(MISCHIEVOUS_SKULL_BLOCK, new Item.Settings())
-		// );
 	}
 }
